4328b9aea4379f050f6478241181b71a6417ccd3
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-04-24 16:32:30 -0400

    finished style and layout for project pages

--- src/routes/ExpandedProject/ExpandedProject.js
+++ src/routes/ExpandedProject/ExpandedProject.js
@@ -26,16 +26,18 @@ export default class ExpandedProject extends Component {
         const parameter = this.props.match.params.projectid
         const currentProject = this.props.projects[parameter - 1]
         return (
-            <section className="projectSection">
+            <section className="projectSection projectSingle">
             <h2>{currentProject.name}</h2>
-            <ImageGallery items={ isMobile ? currentProject.screenshots_mobile : currentProject.screenshots_desktop } slideDuration={1000} showBullets={true} showThumbnails={false}/>
-            <p className="projectDescription">{currentProject.description}</p>
             <div className="languages">{currentProject.languageIcons}</div>
+            <p className="projectDescription">{currentProject.description}</p>
             <div className="projectLinks">
                 <a href={currentProject.liveApp} className="projectLink" target="/blank">Live App</a>
                 <a href={currentProject.client} className="projectLink" target="/blank">Client Repo</a>
                 <a href={currentProject.server} className="projectLink" target="/blank">Server Repo</a>
             </div>
+            
+            <h3>Screenshot Gallery</h3>
+            <ImageGallery items={ isMobile ? currentProject.screenshots_mobile : currentProject.screenshots_desktop } slideDuration={1000} showBullets={true} showThumbnails={false}/>
             <div role="button" className="suggestionButton"><Link className="buttonLink" to='/projects'>Go Back to Projects</Link></div>
             </section>
         )
