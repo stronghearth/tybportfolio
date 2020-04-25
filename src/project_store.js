1b94dfcc239972af98236f0a48139e2c2f9e77d0
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-04-25 12:22:01 -0400

    updated project descriptions

--- src/project_store.js
+++ src/project_store.js
@@ -26,7 +26,7 @@ export default {
        "projects": [ {
             id: 1,
             name: 'Canonize',
-            description: 'When you register with Canonize, you can make a record of all the characters who live in your story. Once many characters become a part of your Canon, you can easily find individuals and expand your notes on them if you wish. Canonize is a great tool for fiction writers and tabletop role-playing game masters alike.',
+            description: 'When you register with Canonize, you can make a record of all the characters who live in your story. Once many characters become a part of your Canon, you can easily find individuals and expand your notes on them if you wish. Canonize is a great tool for fiction writers and tabletop role-playing game masters alike. This application was inspired by my hobby of playing Dungeons and Dragons and my love for crafting characters for it. Upcoming feature: users can keep track of relationships between characters.',
             liveApp: 'https://canonize.now.sh/',
             client: 'https://github.com/stronghearth/canonize-client',
             server: 'https://github.com/stronghearth/canonize-server',
@@ -63,7 +63,7 @@ export default {
         {
             id: 2,
             name: 'Catalan Cards',
-            description: 'Learn the Catalan language, exclusive to Barcelona, Spain, through the spaced repetition technique. Words you haven\'t memorized yet will appear more often! Keep track of your overall score and how often you\'ve gotten a word correct!',
+            description: 'Learn the Catalan language, exclusive to Barcelona, Spain, through the spaced repetition technique. Words you haven\'t memorized yet will appear more often! Keep track of your overall score and how often you\'ve gotten a word correct! Me and a partner built this application in order to show my ability to apply an algorithm and use the linked list data type to apply the spaced repetition technique to the application. This app is built with an intuitive flash card interface to where users can quiz themselves on the English translations of Catalan vocabulary words.',
             liveApp: 'https://catalan-repetition.now.sh/',
             client: 'https://github.com/stronghearth/spaced-repetition',
             server: 'https://github.com/jordanxcast/spaced-repetition-api',
@@ -100,7 +100,7 @@ export default {
         {
             id: 3,
             name: 'Indecisio',
-            description: 'Indecisio is a randomized activity suggestion application for people who need suggestions of what to do while stuck in quarantine during COVID-19. Users can add suggestions to the randomized activity pool and build a profile of what activites they have completed the most.',
+            description: 'Indecisio is a randomized activity suggestion application for people who need suggestions of what to do while stuck in quarantine. Users can add suggestions to the randomized activity pool and build a profile of what activities they have completed the most. I worked in a team of three, and we were inspired by the 2020 COVID-19 pandemic and restrictions on going out to public places and social distancing that were in place.',
             liveApp: 'https://indecisio.now.sh/',
             client: 'https://github.com/thinkful-ei-jaguar/indecisio-client',
             server: 'https://github.com/stronghearth/indecisio-server',
